66078234491d5d4fd30d68460b13bd4de49d2232
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2024-11-22 10:45:19 -0500

    Falta Submission

--- CRUD/CRUD/settings.py
+++ CRUD/CRUD/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libreria'
+    'libreria',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
